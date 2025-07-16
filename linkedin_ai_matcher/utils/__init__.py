354eb1bfd02278236915bb582d2fe219ed1b0ec1
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-07-16 16:43:19 -0700

    chore(utils): update exports

--- linkedin_ai_matcher/utils/__init__.py
+++ linkedin_ai_matcher/utils/__init__.py
@@ -1,2 +1,9 @@
 from .log import create_logger as create_logger
 from .interaction import sleep_normal as sleep_normal
+from .text_manipulation import (
+    extract_tag_content as extract_tag_content,
+    normalize_markup as normalize_markup,
+)
+from .conversion import (
+    load_file as load_file,
+)
