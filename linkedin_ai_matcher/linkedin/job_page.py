c689919647a6c475c87aebed4da146723b31edb1
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-07-21 17:32:16 -0700

    feat(JobPageClient): extract company and title

--- linkedin_ai_matcher/linkedin/job_page.py
+++ linkedin_ai_matcher/linkedin/job_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from html_to_markdown import convert_to_markdown
 
 from linkedin_ai_matcher.utils import sleep_normal
+from linkedin_ai_matcher.models import JobContent
 
 from .linkedin_client import LinkedinClient
 
@@ -9,6 +10,7 @@ from .linkedin_client import LinkedinClient
 class JobPageClient(LinkedinClient):
     JOB_DETAILS_ID = "job-details"
     SEE_MORE_BUTTON = "jobs-description__footer-button"
+    SIGN_IN_DISMISS_BUTTON = "modal__dismiss"
 
     def _click_see_more_optional(self) -> None:
         """
@@ -55,7 +57,7 @@ class JobPageClient(LinkedinClient):
 
         self.logger.info(f"Opened job page for ID {job_id}: {job_page_url}")
 
-    def extract_job_description(self) -> str | None:
+    def _extract_job_description(self) -> str | None:
         """
         Extracts the job description from the job page.
         """
@@ -71,3 +73,49 @@ class JobPageClient(LinkedinClient):
             return None
 
         return self._description_to_markdown(job_details_html)
+
+    def _extract_job_title(self) -> str | None:
+        """
+        Extracts the job title from the job page.
+        """
+        try:
+            job_title_container = self.driver.find_element(
+                By.CLASS_NAME, "job-details-jobs-unified-top-card__job-title"
+            )
+            job_title_element = job_title_container.find_element(By.TAG_NAME, "h1")
+        except Exception as e:
+            self.logger.error(f"Failed to extract job title: {e}")
+            return None
+
+        return job_title_element.text.strip()
+
+    def _extract_job_company(self) -> str | None:
+        """
+        Extracts the job company from the job page.
+        """
+        try:
+            job_company_container = self.driver.find_element(
+                By.CLASS_NAME, "job-details-jobs-unified-top-card__company-name"
+            )
+            job_company_element = job_company_container.find_element(By.TAG_NAME, "a")
+        except Exception as e:
+            self.logger.error(f"Failed to extract job company: {e}")
+            return None
+
+        return job_company_element.text.strip()
+
+    def extract_job_content(self) -> JobContent | None:
+        """
+        Extracts the job content from the job page.
+        """
+        job_description = self._extract_job_description()
+        job_title = self._extract_job_title()
+        job_company = self._extract_job_company()
+
+        if not job_description or not job_title or not job_company:
+            self.logger.warning("Failed to extract complete job content.")
+            return None
+
+        return JobContent(
+            description=job_description, title=job_title, company=job_company
+        )
