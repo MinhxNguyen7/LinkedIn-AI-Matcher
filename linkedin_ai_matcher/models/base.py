c82049603110427f672bbff8c2a8b3fa97e9c6a3
MinhxNguyen7 <64875104+MinhxNguyen7@users.noreply.github.com>
2025-07-24 22:21:50 -0700

    feat(Model): save and load from json

--- linkedin_ai_matcher/models/base.py
+++ linkedin_ai_matcher/models/base.py
@@ -2,12 +2,35 @@
 Base classes for models
 """
 
+from pathlib import Path
+from typing import Type, TypeVar
+
 from pydantic import BaseModel
 
 from linkedin_ai_matcher.utils.text_manipulation import normalize_markup
 
+ModelSubclass = TypeVar("ModelSubclass", bound="Model")
+
 
 class Model(BaseModel):
+    def save(self, path: Path | str):
+        if isinstance(path, str):
+            path = Path(path)
+        # Save the model data to the specified path
+        with open(path, "w") as f:
+            f.write(self.model_dump_json())
+
+    @classmethod
+    def load(cls: Type[ModelSubclass], path: Path | str) -> ModelSubclass:
+        """Load a Model subclass from a JSON file with type validation."""
+        if isinstance(path, str):
+            path = Path(path)
+
+        with open(path, "r") as f:
+            json_data = f.read()
+
+        return cls.model_validate_json(json_data)
+
     def __str__(self) -> str:
         """
         Convert the data to an HTML-like markup format.
